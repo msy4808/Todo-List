118eae92b574811a892753e2b7ea96809e389fc0
문 성 운 <mkik90911@naver.com>
2022-01-23 22:52:55 +0900

    Input TEST Toast Log

--- app/src/main/java/android/moon/todolist/MainActivity.java
+++ app/src/main/java/android/moon/todolist/MainActivity.java
@@ -3,12 +3,21 @@ package android.moon.todolist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextInputEditText in;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        in = findViewById(R.id.input);
+    }
+    public void enterClick(View v){
+        String str = in.getText().toString();
+        Toast.makeText(getApplicationContext(), str,Toast.LENGTH_LONG).show();
     }
 }
