93a79c8a109021acef3fd3024e1233fb3a0aeb7a
문 성 운 <mkik90911@naver.com>
2022-01-25 23:03:14 +0900

    onClick Dialog

--- app/src/main/java/android/moon/todolist/MainActivity.java
+++ app/src/main/java/android/moon/todolist/MainActivity.java
@@ -1,9 +1,11 @@
 package android.moon.todolist;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -25,7 +27,19 @@ public class MainActivity extends AppCompatActivity {
         adapter.setOnItemClickListener(new ListAdapter.onItemClickListener() {
             @Override
             public void onItemCLick(View v, int position) {
-                Toast.makeText(getApplicationContext(), str_list.get(position), Toast.LENGTH_LONG).show();
+                new AlertDialog.Builder(MainActivity.this) // TestActivity 부분에는 현재 Activity의 이름 입력.
+                        .setMessage(str_list.get(position) + " 를 삭제하시겠습니까?")     // 제목 부분 (직접 작성)
+                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {      // 버튼1 (직접 작성)
+                            public void onClick(DialogInterface dialog, int which){
+                                Toast.makeText(getApplicationContext(), "확인 누름", Toast.LENGTH_SHORT).show(); // 실행할 코드
+                            }
+                        })
+                        .setNegativeButton("취소", new DialogInterface.OnClickListener() {     // 버튼2 (직접 작성)
+                            public void onClick(DialogInterface dialog, int which){
+                                Toast.makeText(getApplicationContext(), "취소 누름", Toast.LENGTH_SHORT).show(); // 실행할 코드
+                            }
+                        })
+                        .show();
             }
         });
     }
