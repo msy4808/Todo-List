40510974765ee455e1aa96f3383f7b751fb3e32c
문 성 운 <mkik90911@naver.com>
2022-01-26 16:32:47 +0900

    EnterKey Event Rollback

--- app/src/main/java/android/moon/todolist/MainActivity.java
+++ app/src/main/java/android/moon/todolist/MainActivity.java
@@ -5,16 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity { //쉐이드프리퍼런스가 아닌 DB사용예정
     TextInputEditText in;
     ArrayList<String> str_list = new ArrayList<String>();
     ListAdapter adapter;
@@ -27,26 +31,26 @@ public class MainActivity extends AppCompatActivity {
         adapter = new ListAdapter(str_list);
         adapter.setOnItemClickListener(new ListAdapter.onItemClickListener() {
             @Override
-            public void onItemCLick(View v, int position) { //확인 눌렀을때 데이터 삭제
-                new AlertDialog.Builder(MainActivity.this) // TestActivity 부분에는 현재 Activity의 이름 입력.
-                        .setMessage(str_list.get(position) + " 를 삭제하시겠습니까?")     // 제목 부분 (직접 작성)
-                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {      // 버튼1 (직접 작성)
+            public void onItemCLick(View v, int position) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setMessage(str_list.get(position) + " 를 삭제하시겠습니까?")
+                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int which){
                                 str_list.remove(position);
                                 viewlist.setAdapter(adapter);
-                                Toast.makeText(getApplicationContext(), "삭제하였습니다", Toast.LENGTH_SHORT).show(); // 실행할 코드
+                                Toast.makeText(getApplicationContext(), "삭제하였습니다", Toast.LENGTH_SHORT).show();
                             }
                         })
                         .setNegativeButton("취소", new DialogInterface.OnClickListener() {     // 버튼2 (직접 작성)
                             public void onClick(DialogInterface dialog, int which){
-                                Toast.makeText(getApplicationContext(), "취소 누름", Toast.LENGTH_SHORT).show(); // 실행할 코드
+                                Toast.makeText(getApplicationContext(), "취소 누름", Toast.LENGTH_SHORT).show();
                             }
                         })
                         .show();
             }
         });
     }
-    public void enterClick(View v){ //엔터키 눌렸을때 실행되도록 추가
+    public void btnClick(View v){
         String str = in.getText().toString();
         str_list.add(str);
         viewlist = findViewById(R.id.list);
